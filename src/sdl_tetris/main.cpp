754c580eaa14f1890e6771178f48f849f07f90f2
Nabiel Kandiel <nabkan5@gmail.com>
2026-06-14 21:52:16 -0400

    use ball sprite instead

--- src/sdl_tetris/main.cpp
+++ src/sdl_tetris/main.cpp
@@ -4,12 +4,10 @@
 #include "SDL3/SDL_keyboard.h"
 #include "SDL3/SDL_keycode.h"
 #include "SDL3/SDL_pixels.h"
-#include "SDL3/SDL_rect.h"
 #include "SDL3/SDL_render.h"
 #include "SDL3/SDL_stdinc.h"
 #include "SDL3/SDL_video.h"
 #include "sprites.hpp"
-#include "texture.hpp"
 #include "utility.h"
 
 int main()
@@ -22,11 +20,11 @@ int main()
     // surface in the window
     SDL_Renderer *sdl_renderer{nullptr};
     // character sprite
-    Sprites character_texture(
-        {Sprites::Direction::LEFT, Sprites::Direction::UP, Sprites::Direction::DOWN, Sprites::Direction::RIGHT});
-    Texture ball_texture{SDL_Color{.r = 0, .g = 179, .b = 180, .a = 0}};
+    Sprites ball_sprite(
+        {Sprites::Direction::LEFT, Sprites::Direction::UP, Sprites::Direction::DOWN, Sprites::Direction::RIGHT},
+        SDL_Color{.r = 0, .g = 180, .b = 180, .a = 0});
 
-    character_texture.sliceTextureHorizontal(105.0F, 420.0F);
+    ball_sprite.sliceTextureBox(32.0F, 32.0F);
 
     SDL_Log("Starting with dimension %u x %u\n", k_screen_width, k_screen_height);
     if (!init(&sdl_window, &sdl_renderer, k_screen_width, k_screen_height)) {
@@ -34,21 +32,16 @@ int main()
         return 1;
     }
 
-    if (!loadMedia(character_texture.texture, sdl_renderer, "arrows.png")) {
+    if (!loadMedia(ball_sprite.texture, sdl_renderer, "balls.png")) {
         SDL_Log("failed to load media\n");
         return 2;
-    }
-
-    if (!loadMedia(ball_texture, sdl_renderer, "ball.png")) {
-        SDL_Log("falied to load media\n");
-        return 3;
     }
 
     bool quit{false};
     SDL_Event event;
     SDL_zero(event);
 
-    character_texture.active_dir = Sprites::Direction::UP;
+    ball_sprite.active_dir = Sprites::Direction::UP;
     // default background to white
     SDL_Color bg_color{.r = 0xFF, .g = 0xFF, .b = 0xFF, .a = 0xFF};
 
@@ -60,16 +53,16 @@ int main()
             } else if (event.type == SDL_EVENT_KEY_DOWN) {
                 switch (event.key.key) {
                 case SDLK_UP:
-                    character_texture.active_dir = Sprites::Direction::UP;
+                    ball_sprite.active_dir = Sprites::Direction::UP;
                     break;
                 case SDLK_DOWN:
-                    character_texture.active_dir = Sprites::Direction::DOWN;
+                    ball_sprite.active_dir = Sprites::Direction::DOWN;
                     break;
                 case SDLK_LEFT:
-                    character_texture.active_dir = Sprites::Direction::LEFT;
+                    ball_sprite.active_dir = Sprites::Direction::LEFT;
                     break;
                 case SDLK_RIGHT:
-                    character_texture.active_dir = Sprites::Direction::RIGHT;
+                    ball_sprite.active_dir = Sprites::Direction::RIGHT;
                     break;
                 default:
                     break;
@@ -107,18 +100,16 @@ int main()
         SDL_SetRenderDrawColor(sdl_renderer, bg_color.r, bg_color.g, bg_color.b, 0xFF);
         SDL_RenderClear(sdl_renderer);
         // render image to screen
-        const SDL_FRect src_rect = character_texture.getActiveRect();
-        float render_x = (k_screen_width - src_rect.w) / 2.F;
-        float render_y = (k_screen_height - src_rect.h) / 2.F;
-        character_texture.texture.render(render_x, render_y, sdl_renderer, &src_rect);
+        float render_x = k_screen_width / 2.F;
+        float render_y = k_screen_height / 2.F;
+        ball_sprite.renderActive(sdl_renderer, render_x, render_y);
 
         // render ball
-        ball_texture.render(0.F, 0.F, sdl_renderer);
 
         // update screen
         SDL_RenderPresent(sdl_renderer);
     }
 
-    close(&sdl_window, character_texture.texture, &sdl_renderer);
+    close(&sdl_window, ball_sprite.texture, &sdl_renderer);
     return 0;
 }
--- src/sdl_tetris/sprites.hpp
+++ src/sdl_tetris/sprites.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <concepts>
 #include <span>
 #include <utility>
 #include "texture.hpp"
@@ -21,7 +22,8 @@ struct Sprites
     Direction active_dir = Direction::UP;
 
     // passed is (index in png -> direction it represents)
-    Sprites(std::array<Direction, 4> dir_map) : dir_map(dir_map) {}
+    Sprites(std::array<Direction, 4> dir_map, SDL_Color key) : dir_map(dir_map), texture(key) {}
+    Sprites(std::array<Direction, 4> dir_map) : dir_map(dir_map), texture(std::nullopt) {}
     ~Sprites() = default;
     // dont allow this struct to be copied or moved
     Sprites(const Sprites &) = delete;            // Copy Constructor
@@ -29,9 +31,7 @@ struct Sprites
     Sprites(Sprites &&) = delete;                 // Move Constructor
     Sprites &operator=(Sprites &&) = delete;      // Move Assignment
 
-    template <typename T>
-        requires std::same_as<T, float>
-    void sliceTextureHorizontal(T spr_w, T spr_h)
+    void sliceTextureHorizontal(std::same_as<float> auto spr_w, std::same_as<float> auto spr_h)
     {
         std::span<SDL_FRect, 4> view{sprite_rects};
         size_t idx = 0;
@@ -39,6 +39,31 @@ struct Sprites
             view[std::to_underlying(dir)] = {static_cast<float>(idx) * spr_w, 0.0F, spr_w, spr_h};
             idx++;
         }
+    }
+
+    void sliceTextureBox(std::same_as<float> auto spr_w, std::same_as<float> auto spr_h)
+    {
+        std::span<SDL_FRect, 4> view{sprite_rects};
+        size_t idx = 0;
+        size_t idy = 0;
+        for (const auto &dir : dir_map) {
+            view[std::to_underlying(dir)] = {static_cast<float>(idx) * spr_w, static_cast<float>(idy) * spr_h, spr_w,
+                                             spr_h};
+            if (idx == 1) {
+                idx = 0;
+                idy = 1;
+            } else {
+                idx++;
+            }
+        }
+    }
+
+    void renderActive(SDL_Renderer *sdl_renderer, std::same_as<float> auto center_x, std::same_as<float> auto center_y)
+    {
+        const auto src_rect = getActiveRect();
+        float render_x = center_x - (src_rect.w / 2.F);
+        float render_y = center_y - (src_rect.h / 2.F);
+        texture.render(render_x, render_y, sdl_renderer, &src_rect);
     }
 
     [[nodiscard]] const SDL_FRect &getActiveRect() const
